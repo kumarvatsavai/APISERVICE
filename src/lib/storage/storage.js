43571301f18fb2ef7a516d5c40fa14f8cce47649
hai.t.le16@gmail.com <hai.t.le16@gmail.com>
2018-11-27 21:40:28 -0800

    fixed random bugs

--- src/lib/storage/storage.js
+++ src/lib/storage/storage.js
@@ -1,21 +1,6 @@
 'use strict';
 
-import memoryStorage from './memory.js';
-import fileStorage from './filesystem.js';
 import mongoStorage from './mongo.js';
 
-let dataStorageModule = {};
+export default mongoStorage;
 
-switch( process.env.STORAGE ) {
-  case 'filesystem':
-    dataStorageModule = fileStorage;
-    break;
-  case 'mongo':
-    dataStorageModule = mongoStorage;
-    break;
-  default:
-    dataStorageModule = memoryStorage;
-    break;
-}
-
-export default dataStorageModule;

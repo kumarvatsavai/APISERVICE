f30c343b1c688cba68c6b5d2f33e17f135ea9fc6
Kevinoh47 <kohok47@gmail.com>
2018-12-04 20:56:50 -0800

    tiny updates

--- src/auth/model.js
+++ src/auth/model.js
@@ -21,7 +21,8 @@ const userSchema = new mongoose.Schema(
 
 function _getDefaultCapabilities (role) {
   return role === 'gamer' ? ['read','create','update']
-    :role === 'admin' ? ['create','read','update','delete'] :['read'];
+    :role === 'admin' ? ['create','read','update','delete']
+      :['read'];
 }
 
 userSchema.virtual('acl', {
